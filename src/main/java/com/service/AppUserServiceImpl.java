f00447c01d0f2087f6dde60465de878b71886579
jjlrevature <69030071+techFries52@users.noreply.github.com>
2022-08-19 15:44:41 -0500

    override UserDetails

--- src/main/java/com/service/AppUserServiceImpl.java
+++ src/main/java/com/service/AppUserServiceImpl.java
@@ -6,16 +6,39 @@ import com.repo.AppUserRepo;
 import com.repo.RoleRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class AppUserServiceImpl implements AppUserService {
+public class AppUserServiceImpl implements AppUserService, UserDetailsService {
 
     private final AppUserRepo appUserRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = appUserRepo.findByUsername(username);
+        if(user == null){
+            log.error("user not found in database");
+            throw new UsernameNotFoundException("user not found in databaseee");
+        } else {
+            log.info("user {} found in the database!", username);
+        }
+        Collection<SimpleGrantedAuthority> authorites = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorites.add(new SimpleGrantedAuthority(role.getName()));
+        });
+
+        return new org.springframework.security.core.userdetails.User(user.getName(),user.getPassword(), authorities);
+    }
 
     @Override
     public AppUser saveUser(AppUser appUser) {
@@ -48,4 +71,5 @@ public class AppUserServiceImpl implements AppUserService {
         log.info("fetching all users");
         return appUserRepo.findAll();
     }
+
 }
