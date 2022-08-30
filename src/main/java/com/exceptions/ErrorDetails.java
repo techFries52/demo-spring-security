0d2cef2b930c6b6b504ddd8791467bed967f6b1e
Jesse Lauesen <69030071+techFries52@users.noreply.github.com>
2022-08-30 13:54:40 -0500

    started implementing custom exception handler
    need to work around using the security authenitcation filter

---
+++ src/main/java/com/exceptions/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ErrorDetails {
+
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+
+}
--- src/main/java/com/util/TokenWriter.java
+++ src/main/java/com/util/TokenWriter.java
@@ -4,15 +4,17 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.models.AppUser;
 import com.models.Role;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
 import java.util.stream.Collectors;
-
+@Component
 public class TokenWriter {
 
 
